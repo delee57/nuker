774a56c889bdf40651099b3ed7b7c57cec145519
Rdimo <80375661+Rdimo@users.noreply.github.com>
2021-09-27 12:09:34 +0200

    Update install_requirements.cmd

--- install_requirements.cmd
+++ install_requirements.cmd
@@ -1,1 +1,1 @@
-pip install requests selenium bs4 colorama undetected_chromedriver tqdm datetime pillow
+pip install requests selenium bs4 colorama undetected_chromedriver tqdm datetime pillow pyinstaller
